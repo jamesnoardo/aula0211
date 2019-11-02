213bb5d72708e75fbffc5447d61dac33b9f45023
jamesnoardo <jamesnoardo@users.noreply.github.com>
2019-11-02 11:04:53 -0300

    Porque nós queremos

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <h1>Quem é o James</h1>
+      <p> É O amigo de todos </p>
+      <p> Ele é um cara animado</p>
+
+
+
+
+
+
+    </div>
+  );
+}
+
+export default App;
